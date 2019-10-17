6b25787e3d22cf868a5e813495de1c268f2d9c99
Starian Chen (Shang Hai Wei Chuang Ruan Jian) <v-stache@microsoft.com>
2019-10-17 21:20:50 +0800

    Add project files.

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestStack.White;
+using TestStack.White.Configuration;
+using TestStack.White.Factory;
+using TestStack.White.UIItems;
+using TestStack.White.UIItems.Finders;
+using TestStack.White.UIItems.WindowItems;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        public TestContext TestContext { get; set; }
+
+        private static TestContext _testContext;
+
+        [ClassInitialize]
+        public static void SetupTests(TestContext testContext)
+        {
+            _testContext = testContext;
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var applicationDirectory = TestContext.TestRunDirectory;
+            Console.WriteLine(applicationDirectory);
+
+            var applicationPath = Path.Combine(@"C:\Users\staraich\source\repos\WpfApp1\", "WpfApp1.exe");
+            Application application = Application.Launch(applicationPath);
+            Window window = application.GetWindow("MainWindow", InitializeOption.NoCache);
+            TextBox tb= window.Get<TextBox>("textBox");
+            Button button = window.Get<Button>("button");
+            button.Click();
+            
+        }
+    }
+}
